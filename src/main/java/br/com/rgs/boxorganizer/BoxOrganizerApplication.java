9bad83722ef1d822758913c66f704470cc4832bb
Rafael Garcia SantAnna <rgarciasantanna@gmail.com>
2024-04-20 13:07:09 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/rgs/boxorganizer/BoxOrganizerApplication.java
@@ -0,0 +1,13 @@
+package br.com.rgs.boxorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rgs/boxorganizer/BoxOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rgs.boxorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
